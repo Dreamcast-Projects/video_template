fbcb932ac10f223153a2addc2db44eeae7e648c3
Andress Barajas <andress.barajas@gmail.com>
2023-03-19 22:10:34 -0700

    Example of it in use (main.c)

--- main.c
+++ main.c
@@ -1,9 +1,27 @@
 #include <kos.h>
 
+#include "format-player.h"
+
 int main()
 {
     vid_set_mode(DM_640x480_NTSC_IL, PM_RGB565);
     pvr_init_defaults();
+
+    format_player_t* player = player_create("/rd/roguelogo.roq", 0);
+
+    player_play(player, 0);
+    // Decode
+    // do {
+    //     if(controller_state(CONT_START))
+    //         break;
+
+    //     if(controller_state(CONT_A))
+    //         ;
+	
+    //     // Decode
+    //     player_decode(player);
+    // } while (!player_has_ended(player));
+
 
     return 0;
 }
